81fa1c3eeb12537d0ec740ab7b815e0e9946cf73
ddouser <44279659+ddouser@users.noreply.github.com>
2023-12-15 17:43:11 +0700

    добавил класс user и userDTO для авторизации

--- src/main/java/rest/dto/UserDTO.java
+++ src/main/java/rest/dto/UserDTO.java
@@ -1,1 +1,20 @@
-=
+package rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String id;
+    private String password;
+    private String username;
+    private String birthDate;
+
+}
